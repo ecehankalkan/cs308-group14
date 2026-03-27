3ab0e35afce5978a1e0a00cdf044b1d791214226
Mazen Zeybek <mazenalzaibak@gmail.com>
2026-03-27 21:57:48 +0300

    setup firebase dependencies and initialize app

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'pages/home_page.dart';
 import 'pages/login_page.dart';
 import 'pages/signup_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBNeHEKwUkVlkekmE-9KOU5ttS_cPlppSA",
+      authDomain: "inkcloudsu.firebaseapp.com",
+      projectId: "inkcloudsu",
+      storageBucket: "inkcloudsu.firebasestorage.app",
+      messagingSenderId: "750510641792",
+      appId: "1:750510641792:web:62ba12c0c508120f8d4c65",
+    ),
+  );
   runApp(const MyApp());
 }
 
